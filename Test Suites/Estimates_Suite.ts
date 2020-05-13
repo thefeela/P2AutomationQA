8e0bfb027afe354104f03c0f6cef5d9e05983327
sushmi.ponraj <sushmi.ponraj@10.47.115.22>
2020-05-13 18:08:35 +0530

    Estimates Folder Structure Added

---
+++ Test Suites/Estimates_Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Estimates_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d2a5f999-53cd-4fc2-9343-9a3d9f0ed4a5</testSuiteGuid>
+</TestSuiteEntity>
